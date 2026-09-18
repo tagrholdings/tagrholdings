008a859792b5827650bc99d02d41eb7b2c661eda
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2026-09-18 03:50:42 -0300

    feat: update VaultContent component to adjust maximum width for improved layout

--- components/ui/vault.tsx
+++ components/ui/vault.tsx
@@ -158,7 +158,7 @@ const VaultContent = React.forwardRef<
                         ? "bottom-2 inset-x-2 w-auto rounded-lg after:hidden! mt-24"
                         : "bottom-0 inset-x-0 w-full rounded-t-lg mt-24",
 
-                    "sm:bottom-6 sm:inset-x-0 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:after:hidden! sm:mt-0 max-h-[90vh]",
+                    "sm:bottom-6 sm:inset-x-0 sm:mx-auto sm:w-full sm:max-w-xl sm:rounded-lg sm:after:hidden! sm:mt-0 max-h-[90vh]",
 
                     className,
                 )}
